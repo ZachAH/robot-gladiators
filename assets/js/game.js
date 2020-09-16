2463319f698362765f91a5a7855e553de26c506e
ZMAN <zachh@github.com>
2020-09-16 15:59:00 -0500

    problem 98

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,7 @@ var fight = function (enemyName) {
   //if player chooses to fight, then fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
 
-    while (enemyHealth > 0) {
+    while (enemyHealth > 0 && playerHealth > 0) {
       //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
       enemyHealth = enemyHealth - playerAttack;
       // Log a resulting message to the console so we know that it worked.
@@ -31,7 +31,9 @@ var fight = function (enemyName) {
       // check enemy's health
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-      } else {
+        break;
+      }
+      else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
       // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
@@ -43,21 +45,25 @@ var fight = function (enemyName) {
       // check player's health
       if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
-      } else {
+        break;
+      }
+      else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
       }
-    }
-  }
+    } 
     //if player choses to skip
-    else if (promptFight === "skip" || promptFight === "SKIP") {
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
     //confirm user wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     //if yes(true), leave fight
     if (confirmSkip) {
       window.alert(playerName + "has decided to skip this fight. Goodbye!");
       //subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 2;
+      playerMoney = playerMoney - 10;
+      console.log("playerMoney", playerMoney);
+      break;
     }
+    
     //if no (false), ask question again by running fight() again
     else {
       fight();
@@ -66,6 +72,7 @@ var fight = function (enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 }
+
 
 for (var i = 0; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
